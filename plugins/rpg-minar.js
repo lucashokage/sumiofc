993a03fa4cc850cf31afcd574173ffed199a6ec5
lucashokage <lucashokage25@gmail.com>
2025-05-11 21:53:02 -0500

    Update rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -3,43 +3,50 @@ let cooldowns = {}
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   let user = global.db.data.users[m.sender]
   
-  let tiempoEspera = 5 * 60 * 1000
+  let tiempoEspera = 600000
   if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera) {
-    let tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera - Date.now()) / 1000))
-    return conn.reply(m.chat, `⏱️ Espera *${tiempoRestante}* para volver a minar.`, m)
+    let tiempoRestante = msToTime(cooldowns[m.sender] + tiempoEspera - Date.now())
+    return conn.reply(m.chat, `《✧》 Debes esperar ${tiempoRestante} para volver a minar.`, m)
   }
   
   cooldowns[m.sender] = Date.now()
   
-  let randomDiamonds = Math.floor(Math.random() * 10) + 1
-  let randomExp = Math.floor(Math.random() * 500) + 100
-  let randomCoins = Math.floor(Math.random() * 500) + 100
+  let coin = pickRandom([1000, 1500, 2000, 900, 800, 1230, 2500, 1200, 1900, 899, 2700])
+  let hasil = Math.floor(Math.random() * 1000)
   
-  user.diamonds += randomDiamonds
-  user.exp += randomExp
-  user.coin += randomCoins
+  user.coin += coin
+  user.exp += hasil
   
-  let mensaje = `
-╭━━━━━━━━━⬣
-┃ ✅ Minaste exitosamente!
-┃ *${randomDiamonds}* 💎 Diamantes
-┃ *${randomExp}* ⚡ XP
-┃ *${randomCoins}* ${global.moneda} Monedas
-╰━━━━━━━━━⬣`
+  let info = `❀ *Te has adentrando en lo profundo de las cuevas*\n\n` +
+  `> *❀ Obtuviste estos recursos ❀*\n\n` +
+  `✩ *Exp*: ${hasil}\n` +
+  `⛀ *${global.moneda}*: ${coin}\n`
   
-  conn.reply(m.chat, mensaje, m)
+  await conn.reply(m.chat, info, m)
+  await m.react('⛏️')
 }
 
 handler.help = ['minar']
-handler.tags = ['rpg']
-handler.command = ['minar', 'minardiamantes', 'mine']
-handler.group = true
+handler.tags = ['economy']
+handler.command = ['minar', 'miming', 'mine']
 handler.register = true
+handler.group = true
 
 export default handler
 
-function segundosAHMS(segundos) {
-  let minutos = Math.floor(segundos / 60)
-  let segundosRestantes = segundos % 60
-  return `${minutos} minutos y ${segundosRestantes} segundos`
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}
+
+function msToTime(duration) {
+  var milliseconds = parseInt((duration % 1000) / 100),
+  seconds = Math.floor((duration / 1000) % 60),
+  minutes = Math.floor((duration / (1000 * 60)) % 60),
+  hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+
+  hours = (hours < 10) ? '0' + hours : hours
+  minutes = (minutes < 10) ? '0' + minutes : minutes
+  seconds = (seconds < 10) ? '0' + seconds : seconds
+
+  return minutes + ' m y ' + seconds + ' s '
 }
