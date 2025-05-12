797d0e0f9ab7053426b9eb37d3ab8cd8eee99d50
lucashokage <lucashokage25@gmail.com>
2025-05-12 01:34:05 -0500

    Update jadibot-setname.js

--- plugins/jadibot-setname.js
+++ plugins/jadibot-setname.js
@@ -1,27 +1,24 @@
 import ws from "ws";
 
 let handler = async (m, { conn, args, text, command, usedPrefix, isOwner }) => {
-    const users = [...new Set(
-        global.conns
-            .filter(conn => conn.user && conn.ws?.socket && conn.ws.socket.readyState !== ws.CLOSED)
-            .map(conn => conn.user.jid)
-    )];
-
-    let isSubbot = users.includes(m.sender);
-    if (!isSubbot && !isOwner) return m.reply("Solo un subbot autorizado puede usar este comando.");
+    let isSubbotOwner = conn.user.jid === m.sender;
+    if (!isSubbotOwner && !isOwner) {
+        return m.reply("⚠️ Solo el owner del bot o el número asociado a este subbot pueden usar este comando.");
+    }
 
     if (!text) {
         return m.reply(`🌲 Por favor especifica el nuevo nombre del bot.`);
     }
-global.db.data.settings[conn.user.jid].botName = text
-let cap = `
+
+    global.db.data.settings[conn.user.jid].botName = text;
+    let cap = `
 ≡ 🌴 Se ha cambiado con éxito el nombre para @${conn.user.jid.split("@")[0]}
 
 🌿 Nuevo nombre : ${text}
 `;
-conn.reply(m.chat, cap, m, { mentions: conn.parseMention(cap)})
-
+    conn.reply(m.chat, cap, m, { mentions: conn.parseMention(cap) });
 }
+
 handler.tags = ["serbot"];
 handler.help = handler.command = ["setname"];
 export default handler;
