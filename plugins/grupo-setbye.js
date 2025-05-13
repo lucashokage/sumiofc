09db66fbc7bdba869777d457d0098a9eae274c31
lucashokage <lucashokage25@gmail.com>
2025-05-13 02:19:06 -0500

    Update grupo-setbye.js

--- plugins/grupo-setbye.js
+++ plugins/grupo-setbye.js
@@ -1,15 +1,20 @@
-let handler = async (m, { conn, text, isRowner }) => {
-  if (!text) return m.reply(`${emoji} Por favor, proporciona un mensaje de despedida para el bot.\n> Ejemplo: #setbye adios user`);
+const handler = async (m, { conn, text, isROwner, isOwner }) => {
+  if (text) {
+    // Asegurarse de que la estructura de datos existe
+    if (!global.db.data.chats[m.chat]) global.db.data.chats[m.chat] = {}
 
-  global.db.data.settings[conn.user.jid].bye = text.trim();
-  
-  m.reply(`${emoji} La despedida del bot ha sido cambiado a: ${global.welcom2}`);
-};
+    // Guardar el mensaje de despedida personalizado
+    global.db.data.chats[m.chat].sBye = text
+    m.reply(`✅ Mensaje de despedida actualizado correctamente`)
+  } else {
+    throw `✳️ Por favor, ingresa el texto para el mensaje de despedida\n\nEjemplo: .setbye Gracias por estar con nosotros`
+  }
+}
 
-handler.help = ['setdespedida'];
-handler.tags = ['tools'];
-handler.command = ['setbye'];
-handler.owner = false;
-handler.admin = true;
+handler.help = ["setbye <texto>"]
+handler.tags = ["group"]
+handler.command = ["setbye"]
+handler.admin = true
+handler.group = true
 
-export default handler;
+export default handler
