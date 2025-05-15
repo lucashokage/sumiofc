0ab8af1a2197ed8ca8aef204263b13e93049739e
lucashokage <lucashokage25@gmail.com>
2025-05-15 00:29:37 -0500

    Update rpg-trabajar.js

--- plugins/rpg-trabajar.js
+++ plugins/rpg-trabajar.js
@@ -2,26 +2,21 @@ let cooldowns = {}
 
 let handler = async (m, { conn, isPrems }) => {
   let user = global.db.data.users[m.sender]
-  let tiempoEspera = 5 * 60 * 1000
+  let tiempo = 5 * 60 * 1000
   
-  if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera) {
-    let tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera - Date.now()) / 1000))
-    return conn.reply(m.chat, `⏱️ Espera *${tiempoRestante}* para volver a trabajar.`, m)
+  if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo) {
+    const tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo - Date.now()) / 1000))
+    return conn.reply(m.chat, `⏱️ Espera *${tiempo2}* para volver a trabajar.`, m)
   }
   
-  cooldowns[m.sender] = Date.now()
   let rsl = Math.floor(Math.random() * 500)
-  let emoji = '✅'
-  let moneda = global.moneda || '$'
+  cooldowns[m.sender] = Date.now()
   
   user.coin += rsl
   
-  let mensaje = `
-╭━━━━━━━━━⬣
-┃ ${emoji} ${pickRandom(trabajo)} *${toNum(rsl)}* ( *${rsl}* ) ${moneda} ⛀
-╰━━━━━━━━━⬣`
-  
-  conn.reply(m.chat, mensaje, m)
+  let emoji = '💰'
+  let moneda = global.moneda || 'Coin'
+  await conn.reply(m.chat, `${emoji} ${pickRandom(trabajo)} *${toNum(rsl)}* ( *${rsl}* ) ${moneda} ⛀`, m)
 }
 
 handler.help = ['trabajar']
@@ -57,37 +52,37 @@ function pickRandom(list) {
 }
 
 const trabajo = [
-  "Trabajas como cortador de galletas y ganas",
-  "Trabaja para una empresa militar privada, ganando",
-  "Organiza un evento de cata de vinos y obtienes",
-  "Limpias la chimenea y encuentras",
-  "Desarrollas juegos para ganarte la vida y ganas",
-  "Trabajaste en la oficina horas extras por",
-  "Trabajas como secuestrador de novias y ganas",
-  "Alguien vino y representó una obra de teatro. Por mirar te dieron",
-  "Compraste y vendiste artículos y ganaste",
-  "Trabajas en el restaurante de la abuela como cocinera y ganas",
-  "Trabajas 10 minutos en un Pizza Hut local. Ganaste",
-  "Trabajas como escritor(a) de galletas de la fortuna y ganas",
-  "Revisas tu bolso y decides vender algunos artículos inútiles que no necesitas. Resulta que toda esa basura valía",
-  "Desarrollas juegos para ganarte la vida y ganas",
-  "Trabajas todo el día en la empresa por",
-  "Diseñaste un logo para una empresa por",
-  "¡Trabajó lo mejor que pudo en una imprenta que estaba contratando y ganó su bien merecido!",
-  "Trabajas como podador de arbustos y ganas",
-  "Trabajas como actor de voz para Bob Esponja y te las arreglaste para ganar",
-  "Estabas cultivando y Ganaste",
-  "Trabajas como constructor de castillos de arena y ganas",
-  "Trabajas como artista callejera y ganas",
-  "¡Hiciste trabajo social por una buena causa! por tu buena causa Recibiste",
-  "Reparaste un tanque T-60 averiado en Afganistán. La tripulación te pagó",
-  "Trabajas como ecologista de anguilas y ganas",
-  "Trabajas en Disneyland como un panda disfrazado y ganas",
-  "Reparas las máquinas recreativas y recibes",
-  "Hiciste algunos trabajos ocasionales en la ciudad y ganaste",
-  "Limpias un poco de moho tóxico de la ventilación y ganas",
-  "Resolviste el misterio del brote de cólera y el gobierno te recompensó con una suma de",
-  "Trabajas como zoólogo y ganas",
-  "Vendiste sándwiches de pescado y obtuviste",
-  "Reparas las máquinas recreativas y recibes",
+   "Trabajas como cortador de galletas y ganas",
+   "Trabaja para una empresa militar privada, ganando",
+   "Organiza un evento de cata de vinos y obtienes",
+   "Limpias la chimenea y encuentras",
+   "Desarrollas juegos para ganarte la vida y ganas",
+   "Trabajaste en la oficina horas extras por",
+   "Trabajas como secuestrador de novias y ganas",
+   "Alguien vino y representó una obra de teatro. Por mirar te dieron",
+   "Compraste y vendiste artículos y ganaste",
+   "Trabajas en el restaurante de la abuela como cocinera y ganas",
+   "Trabajas 10 minutos en un Pizza Hut local. Ganaste",
+   "Trabajas como escritor(a) de galletas de la fortuna y ganas",
+   "Revisas tu bolso y decides vender algunos artículos inútiles que no necesitas. Resulta que toda esa basura valía",
+   "Desarrollas juegos para ganarte la vida y ganas",
+   "Trabajas todo el día en la empresa por",
+   "Diseñaste un logo para una empresa por",
+   "¡Trabajó lo mejor que pudo en una imprenta que estaba contratando y ganó su bien merecido!",
+   "Trabajas como podador de arbustos y ganas",
+   "Trabajas como actor de voz para Bob Esponja y te las arreglaste para ganar",
+   "Estabas cultivando y Ganaste",
+   "Trabajas como constructor de castillos de arena y ganas",
+   "Trabajas como artista callejera y ganas",
+   "¡Hiciste trabajo social por una buena causa! por tu buena causa Recibiste",
+   "Reparaste un tanque T-60 averiado en Afganistán. La tripulación te pagó",
+   "Trabajas como ecologista de anguilas y ganas",
+   "Trabajas en Disneyland como un panda disfrazado y ganas",
+   "Reparas las máquinas recreativas y recibes",
+   "Hiciste algunos trabajos ocasionales en la ciudad y ganaste",
+   "Limpias un poco de moho tóxico de la ventilación y ganas",
+   "Resolviste el misterio del brote de cólera y el gobierno te recompensó con una suma de",
+   "Trabajas como zoólogo y ganas",
+   "Vendiste sándwiches de pescado y obtuviste",
+   "Reparas las máquinas recreativas y recibes",
 ]
