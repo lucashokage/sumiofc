d08305c016fa6b35c05d6520875d39ba806aeb73
lucashokage <lucashokage25@gmail.com>
2025-05-15 00:01:36 -0500

    Update convertidor-ibb.js

--- plugins/convertidor-ibb.js
+++ plugins/convertidor-ibb.js
@@ -75,7 +75,7 @@ function formatBytes(bytes, decimals = 2) {
   const dm = decimals < 0 ? 0 : decimals
   const sizes = ['Bytes', 'KB', 'MB', 'GB']
   const i = Math.floor(Math.log(bytes) / Math.log(k))
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm) + ' ' + sizes[i]
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
 
 handler.help = ['ibb']
