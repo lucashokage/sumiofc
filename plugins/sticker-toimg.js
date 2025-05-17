5f455d3ff79199e2a2171a7fd22814f4121dddc9
lucashokage <lucashokage25@gmail.com>
2025-05-17 02:50:31 -0500

    Update sticker-toimg.js

--- plugins/sticker-toimg.js
+++ plugins/sticker-toimg.js
@@ -1,17 +1,28 @@
-import { webp2png } from '../lib/webp2mp4.js'
 
+import { spawn } from 'child_process'
+import { format } from 'util'
 let handler = async (m, { conn, usedPrefix, command }) => {
-    const notStickerMessage = `${emoji} Debes citar un sticker para convertir a imagen.`
-    const q = m.quoted || m
-    const mime = q.mediaType || ''
-    if (!/sticker/.test(mime)) return m.reply(notStickerMessage)
-    const media = await q.download()
-    let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendFile(m.chat, out, 'output.png', null, m)
+ 
+  if (!global.support.convert && !global.support.magick && !global.support.gm) return handler.disabled = true  
+    if (!m.quoted) throw `${emoji} Debes citar un sticker para convertir a imagen.`
+    let q = m.quoted
+    if (/sticker/.test(q.mediaType)) {
+        let sticker = await q.download()
+        if (!sticker) throw sticker
+        let bufs = []
+        const [_spawnprocess, ..._spawnargs] = [...(global.support.gm ? ['gm'] : global.support.magick ? ['magick'] : []), 'convert', 'webp:-', 'png:-']
+        let im = spawn(_spawnprocess, _spawnargs)
+        im.on('error', e => m.reply(format(e)))
+        im.stdout.on('data', chunk => bufs.push(chunk))
+        im.stdin.write(sticker)
+        im.stdin.end()
+        im.on('exit', () => {
+            conn.sendFile(m.chat, Buffer.concat(bufs), 'img.png', `*✅ tu imagen*`, m)
+        })
+    } else throw ``
 }
-
-handler.help = ['toimg (reply)']
+handler.help = ['toimg <sticker>']
 handler.tags = ['sticker']
-handler.command = ['toimg', 'img', 'jpg']
+handler.command = ['toimg', 'jpg', 'aimg'] 
 
 export default handler
