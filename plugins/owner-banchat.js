0315c1462791ce7d6662155bcb60206cdf57a4e0
lucashokage <lucashokage25@gmail.com>
2025-05-17 01:27:03 -0500

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -2,11 +2,11 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    m.reply(`✅ ${mssg.banChat}`)
+    m.reply(`ℹ️ =͟͟͞❀ sᥙmі - sᥲkᥙrᥲsᥲᥕᥲ  ⏤͟͟͞͞★ desactivado en este chat.`)
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
-handler.command = ['banchat', 'chatoff'] 
+handler.command = ['banchat', 'chatoff', 'bot off'] 
 handler.group = true
 
 export default handler
