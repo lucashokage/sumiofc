1b7098df254071f6345d90e597a90c6a7bfc3ea0
lucashokage <lucashokage25@gmail.com>
2025-05-17 01:27:41 -0500

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -3,10 +3,10 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner} ) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    m.reply(`✅ ${mssg.unBanChat}`)   
+    m.reply(`ℹ️ =͟͟͞❀ sᥙmі - sᥲkᥙrᥲsᥲᥕᥲ  ⏤͟͟͞͞★ ya estaba activado en este chat.`)   
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
-handler.command = ['chaton', 'unbanchat'] 
+handler.command = ['chaton', 'unbanchat', 'bot on'] 
 
 export default handler
