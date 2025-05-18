57040b75f0e5b06fb3a4668d526e7d7eae24d53f
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-05-18 04:40:39 -0500

    Actualizar tool chazan.js

--- plugins/tool chazan.js
+++ plugins/tool chazan.js
@@ -16,7 +16,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   let txt = '╭─⬣「 *Whatmusic Tools* 」⬣\n'
       txt += `│  ≡◦ *「✦」 Titulo ∙* ${title}${artists ? `\n│  ≡◦ *✰ Artista ∙* ${artists.map(v => v.name).join(', ')}` : ''}`
       txt += `${album ? `\n│  ≡◦ *❖ Album ∙* ${album.name}` : ''}${genres ? `\n│  ≡◦ *❀ Genero ∙* ${genres.map(v => v.name).join(', ')}` : ''}\n`
-      txt += `│  ≡◦ *🕜 Fecha de lanzamiento ∙* ${release_date}\n`
+      txt += `│  ≡◦ *ⴵ Fecha de lanzamiento ∙* ${release_date}\n`
       txt += `╰─⬣`
      conn.reply(m.chat, txt, m)
   } else return conn.reply(m.chat, `${emoji} Etiqueta un audio o video de poca duración con el comando *${usedPrefix + command}* para ver que música contiene.`, m)
