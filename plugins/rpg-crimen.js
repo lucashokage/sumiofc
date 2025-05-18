35e01db1e18484d6884d57cf95ef90ba5a8fc4bc
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-05-18 02:16:05 -0500

    Actualizar rpg-crimen.js

--- plugins/rpg-crimen.js
+++ plugins/rpg-crimen.js
@@ -8,7 +8,7 @@ let senderName = conn.getName(senderId)
 let tiempo = 5 * 60
 if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
 let tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
-m.reply(`❀ Ya has cometido un Crimen recientemente, espera ⏱️ *${tiempo2}* para cometer tu próximo Crimen y evitar ser atrapado.`)
+m.reply(`《✧》 Ya has cometido un Crimen recientemente, espera ⴵ *${tiempo2}* para cometer tu próximo Crimen y evitar ser atrapado.`)
 return
 }
 cooldowns[m.sender] = Date.now()
@@ -26,7 +26,7 @@ case 0:
 users[senderId].coin += amountTaken
 users[randomUserId].coin -= amountTaken
 conn.sendMessage(m.chat, {
-text: `${global.emoji} ¡Lograste cometer tu crimen con exito!, acabas de robar *${amountTaken} ${global.moneda} ⛀* a @${randomUserId.split("@")[0]}\n\nSe suman *+${amountTaken} ${global.moneda} ⛀* a ${senderName}.`,
+text: `❀ ¡Lograste cometer tu crimen con exito!, acabas de robar *${amountTaken} ${global.moneda} ⛀* a @${randomUserId.split("@")[0]}\n\nSe suman *+${amountTaken} ${global.moneda} ⛀* a ${senderName}.`,
 contextInfo: { 
 mentionedJid: [randomUserId],
 }}, { quoted: m })
@@ -34,14 +34,14 @@ break
 case 1:
 let amountSubtracted = Math.min(Math.floor(Math.random() * (senderCoin - minAmount + 1)) + minAmount, maxAmount)
 users[senderId].coin -= amountSubtracted
-conn.reply(m.chat, `${global.emoji2} No fuiste cuidadoso y te atraparon mientras cometias tu cirme, se restaron *-${amountSubtracted} ${global.moneda} ⛀* a ${senderName}.`, m)
+conn.reply(m.chat, `《✧》 No fuiste cuidadoso y te atraparon mientras cometias tu cirme, se restaron *-${amountSubtracted} ${global.moneda} ⛀* a ${senderName}.`, m)
 break
 case 2:
 let smallAmountTaken = Math.min(Math.floor(Math.random() * (randomUserCoin / 2 - minAmount + 1)) + minAmount, maxAmount)
 users[senderId].coin += smallAmountTaken
 users[randomUserId].coin -= smallAmountTaken
 conn.sendMessage(m.chat, {
-text: `${global.emoji} Lograste cometer tu crimen con exito, pero te descubrieron y solo lograste tomar *${smallAmountTaken} ${global.moneda} ⛀* de @${randomUserId.split("@")[0]}\n\nSe suman *+${smallAmountTaken} ${global.moneda} ⛀* a ${senderName}.`,
+text: `❀ Lograste cometer tu crimen con exito, pero te descubrieron y solo lograste tomar *${smallAmountTaken} ${global.moneda} ⛀* de @${randomUserId.split("@")[0]}\n\nSe suman *+${smallAmountTaken} ${global.moneda} ⛀* a ${senderName}.`,
 contextInfo: { 
 mentionedJid: [randomUserId],
 }}, { quoted: m })
