2f52a881d20363bf15c336f083d819c47ac534da
lucashokage <lucashokage25@gmail.com>
2025-05-19 22:39:40 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,125 +1,82 @@
-console.log('✅ Iniciando...')
-
 import { join, dirname } from 'path'
-import { createRequire } from 'module';
+import { createRequire } from 'module'
 import { fileURLToPath } from 'url'
-import { setupMaster, fork } from 'cluster'
-import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts';
+import boxen from 'boxen'
+import { setupMaster, fork } from 'cluster';
+import { watchFile, unwatchFile } from 'fs';
+import cfonts from 'cfonts'
 import { createInterface } from 'readline'
 import yargs from 'yargs'
-import express from 'express'
 import chalk from 'chalk'
-import path from 'path'
-import os from 'os'
-import { promises as fsPromises } from 'fs'
-
-// https://stackoverflow.com/a/50052194
+console.log('\nIniciando Frieren Bot')
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
-const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
+const require = createRequire(__dirname)
+const { name, description, collaborators, author, version } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
+const subtitleStyle = chalk.white.bold
+const responseStyle = chalk.dim.bold
 
-const app = express()
-const port = process.env.PORT || 8080;
-
-say('Sumi sakurasawa', {
-  font: 'pallet',
-  align: 'center',
-  gradient: ['red', 'magenta']
+let activeCollaborators = ''
+for (const key in collaborators) {
+if (collaborators.hasOwnProperty(key)) {
+activeCollaborators += collaborators[key] + ', '
+}}
+activeCollaborators = activeCollaborators.slice(0, -2);
+cfonts.say('Frieren\AI', {
+align: 'center',           
+gradient: ['red', 'blue'] 
 })
-say(`powered: leonel`, {
-  font: 'console',
-  align: 'center',
-  gradient: ['cyan', 'magenta']
+cfonts.say(description, {
+font: 'console',
+align: 'center',
+gradient: ['blue', 'magenta']
 })
-
-app.listen(port, () => {
-  console.log(chalk.green(`🌐 Puerto ${port} esta abierto`));
-});
-
+const message = `${subtitleStyle('Desarrollado por »')} ${responseStyle('ianalejandrook15x')}
+${subtitleStyle('Versión »')} ${responseStyle(version)}`
+console.log(boxen(message, { padding: 1, margin: 1, borderStyle: 'double', borderColor: 'blue', float: 'center', }))
 var isRunning = false
-
-async function start(file) {
-  if (isRunning) return
-  isRunning = true
-  const currentFilePath = new URL(import.meta.url).pathname
-  let args = [join(__dirname, file), ...process.argv.slice(2)]
-  say([process.argv[0], ...args].join(' '), {
-    font: 'console',
-    align: 'center',
-    gradient: ['red', 'magenta']
-  })
-  setupMaster({
-    exec: args[0],
-    args: args.slice(1),
-  })
-  let p = fork()
-  p.on('message', data => {
-    console.log('[RECEIVED]', data)
-    switch (data) {
-      case 'reset':
-        p.process.kill()
-        isRunning = false
-        start.apply(this, arguments)
-        break
-      case 'uptime':
-        p.send(process.uptime())
-        break
-    }
-  })
-  //---
-  p.on('exit', (_, code) => {
-    isRunning = false
-    console.error('❎ Ocurrió un error inesperado:', code)
-    start('main.js'); //
-
-    if (code === 0) return
-    watchFile(args[0], () => {
-      unwatchFile(args[0])
-      start(file)
-    })
-  })
-
-  //---
-  console.log(chalk.yellow(`🖥️ ${os.type()}, ${os.release()} - ${os.arch()}`));
-  const ramInGB = os.totalmem() / (1024 * 1024 * 1024);
-  console.log(chalk.yellow(`💾 Total RAM: ${ramInGB.toFixed(2)} GB`));
-  const freeRamInGB = os.freemem() / (1024 * 1024 * 1024);
-  console.log(chalk.yellow(`💽 Free RAM: ${freeRamInGB.toFixed(2)} GB`));
-  console.log(chalk.yellow(`📃 Script by lucas`));
-
-  const packageJsonPath = path.join(path.dirname(currentFilePath), './package.json');
-    try {
-    const packageJsonData = await fsPromises.readFile(packageJsonPath, 'utf-8');
-    const packageJsonObj = JSON.parse(packageJsonData);
-    console.log(chalk.blue.bold(`\n📦 Información del Paquete`));
-    console.log(chalk.cyan(`Nombre: ${packageJsonObj.name}`));
-    console.log(chalk.cyan(`Versión: ${packageJsonObj.version}`));
-    console.log(chalk.cyan(`Descripción: ${packageJsonObj.description}`));
-    console.log(chalk.cyan(`Autor: leonel`));
-  } catch (err) {
-    console.error(chalk.red(`❌ No se pudo leer el archivo package.json: ${err}`));
-  }
-
-
-  console.log(chalk.blue.bold(`\n⏰ Hora Actual`));
-  const currentTime = new Date().toLocaleString('es-ES', { timeZone: 'America/Argentina/Buenos_Aires' })
-  //const currentTime = new Date().toLocaleString();
-  console.log(chalk.cyan(`${currentTime}`));
-
-  setInterval(() => {}, 1000);
-
-  
-
-  //----
-  let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-  if (!opts['test'])
-    if (!rl.listenerCount()) rl.on('line', line => {
-      p.emit('message', line.trim())
-    })
-  // console.log(p)
+function start(file) {
+if (isRunning) return
+isRunning = true
+let args = [join(__dirname, file), ...process.argv.slice(2)]
+setupMaster({
+exec: args[0],
+args: args.slice(1),
+})
+let p = fork()
+p.on('message', data => {
+switch (data) {
+case 'reset':
+p.process.kill()
+isRunning = false
+start.apply(this, arguments)
+break
+case 'uptime':
+p.send(process.uptime())
+break
 }
-
-start('main.js')
+})
+p.on('exit', (_, code) => {
+isRunning = false
+console.error('🌸 Error:\n', code)
+process.exit()
+if (code === 0) return
+watchFile(args[0], () => {
+unwatchFile(args[0])
+start(file)
+})
+})
+let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+if (!opts['test'])
+if (!rl.listenerCount()) rl.on('line', line => {
+p.emit('message', line.trim())
+})
+}
+process.on('warning', (warning) => {
+if (warning.name === 'MaxListenersExceededWarning') {
+console.warn('🌸 Se excedió el límite de Listeners en:')
+console.warn(warning.stack)
+}
+})
+start('start.js')
